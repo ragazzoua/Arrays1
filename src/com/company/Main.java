8cbd00ca60445145ebe1b741af003fc3204685d2
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-09-26 15:03:44 +0300

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] myIntArray = new int[10];
+//        myIntArray[0] = 45;
+//        myIntArray[1] = 476;
+//        myIntArray[5] = 50;
+
+        double[] myDoubleArray = new double[10];
+        for (int i = 0; i < 10; i++) {
+            myIntArray[i] = i * 10;
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Element " + i + " value is " + myIntArray[i]);
+        }
+    }
+}
